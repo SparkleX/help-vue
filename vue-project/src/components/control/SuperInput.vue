57a68a9d5230eb7197c033e72dcfc96dbec8f657
SparkleX <xufei.sun@sap.com>
2023-02-09 17:13:00 +0800

    dev

---
+++ vue-project/src/components/control/SuperInput.vue
@@ -0,0 +1,23 @@
+<template>
+	<input v-bind:value="data" v-on:input="onChange" />
+</template>
+<style scoped>
+input {
+	font-weight: 500;
+	font-size: 1rem;
+	width: 100%;
+	height: 2rem;
+	top: 0px;
+}
+</style>
+
+<script>
+export default {
+	props: ["data"],
+	methods: {
+		onChange(event) {
+			this.$emit("update:data", event.target.value);
+		}
+	},
+}
+</script>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<SuperInput v-model:data="CardCode" />
+	<SuperInput v-model:data="CardCode" />
+	<input v-model="CardCode" />
+	<input v-model="CardCode" />
 </template>
+
+<script setup>
+import SuperInput from '../components/control/SuperInput.vue'
+</script>
+
+
+<script>
+export default {
+	data() {
+		return {
+			CardCode: "abc"
+		}
+	}
+}
+</script>
