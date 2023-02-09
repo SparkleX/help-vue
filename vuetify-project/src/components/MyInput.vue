ac5a1cfdc5a317845521c13a5e0c1f9128120559
SparkleX <xufei.sun@sap.com>
2023-02-09 17:40:30 +0800

    dev

---
+++ vuetify-project/src/components/MyInput.vue
@@ -0,0 +1,17 @@
+<template>
+	<v-text-field label="Label" v-bind:modelValue="modelValue" v-on:input="onChange" ></v-text-field>
+</template>
+  
+<script setup>
+//
+</script>
+<script>
+export default {
+	props: ["modelValue"],
+	methods: {
+		onChange(event) {
+			this.$emit("update:modelValue", event.target.value);
+		}
+	}
+}
+</script>  
---
+++ vuetify-project/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-text-field label="Label" v-model="CardCode"></v-text-field>
+	<v-text-field label="Label" v-model="CardCode"></v-text-field>
+	<MyInput v-model="CardCode"/>
+</template>
+
+<script setup>
+import MyInput from '@/components/MyInput.vue'
+</script>
+
+
+<script>
+export default {
+	data() {
+		return {
+			CardCode: "abc"
+		}
+	}
+}
+</script>
