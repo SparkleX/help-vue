4a87a29e610accf5df8fc25e9378dab29ad84361
SparkleX <xufei.sun@sap.com>
2023-02-23 17:42:40 +0800

    dev

--- vuetify-project/src/control/SuperDataTable.vue
+++ vuetify-project/src/control/SuperDataTable.vue
@@ -1,10 +1,60 @@
 <template>
-	<v-data-table :items-per-page="-1" :headers="headers" :items="items" item-value="name"></v-data-table>
+	<v-data-table :items-per-page="-1" :headers="headers" :items="items" item-value="name">
+		<template v-slot:top>
+			<v-toolbar flat>
+				<v-toolbar-title>Details</v-toolbar-title>
+				<v-spacer></v-spacer>
+				<v-dialog v-model="dialog" max-width="500px">
+					<template v-slot:activator="{ props }">
+						<v-btn color="primary" v-bind="props">
+							+ New
+						</v-btn>
+					</template>
+					<v-card>
+						<v-card-title>
+							<span class="text-h5">Title</span>
+						</v-card-title>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn color="primary" variant="text" @click="save">
+								OK
+							</v-btn>
+							<v-btn variant="text" @click="close">
+								Cancel
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-dialog>
+			</v-toolbar>
+		</template>
+	</v-data-table>
 </template>
   
 
 <script>
 export default {
-	props: ["items", "headers"]
+	props: ["items", "headers"],
+	data() {
+		return {
+			dialog: false
+		}
+	},
+	methods: {
+		close() {
+			this.dialog = false
+			this.$nextTick(() => {
+				this.editedItem = Object.assign({}, this.defaultItem)
+				this.editedIndex = -1
+			})
+		},
+		save() {
+			if (this.editedIndex > -1) {
+				//Object.assign(this.desserts[this.editedIndex], this.editedItem)
+			} else {
+				//this.desserts.push(this.editedItem)
+			}
+			this.close()
+		},
+	}
 }
 </script>
