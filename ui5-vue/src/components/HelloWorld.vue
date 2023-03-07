7e2b284f7c78cb3a11904232a8b88b1572cb8f81
SparkleX <xufei.sun@sap.com>
2023-03-07 10:26:04 +0800

    dev

--- ui5-vue/src/components/HelloWorld.vue
+++ ui5-vue/src/components/HelloWorld.vue
@@ -1,20 +1,38 @@
 
 
 <template>
-	<ui5-page id="page" background-design="List" floating-footer  show-footer>
-		<ui5-bar design="Header" slot="header">
-			<ui5-button icon="home" tooltip="Go home" slot="startContent"></ui5-button>
-			<ui5-label>Title</ui5-label>
-			<ui5-button icon="action-settings" tooltip="Go to settings" slot="endContent"></ui5-button>
-		</ui5-bar>
-		<div>
-			123
-			<ui5-label id="myLabel" for="myInput" required show-colon>First name</ui5-label>
-			<ui5-input id="myInput" required placeholder="Enter your name"></ui5-input>
-			<super-input v-model="a"></super-input>
-			<super-input v-model="a"></super-input>
-			<super-button label="Test" @click="onClick"></super-button>
-		</div>
+	<ui5-page id="page" background-design="List" style="height: 98vh" floating-footer show-footer>
+		<ui5-shellbar id="shellbar" primary-title="SAP Business One" secondary-title="Sales Orders"
+			notifications-count="99+" show-product-switch slot="header">
+
+			<ui5-li slot="menuItems">Application 1</ui5-li>
+			<ui5-li slot="menuItems">Application 2</ui5-li>
+			<ui5-li slot="menuItems">Application 3</ui5-li>
+			<ui5-li slot="menuItems">Application 4</ui5-li>
+			<ui5-li slot="menuItems">Application 5</ui5-li>
+
+		</ui5-shellbar>
+		<ui5-tabcontainer>
+			<ui5-tab text="General" selected>
+				<div>
+					<ui5-label id="myLabel" for="myInput" required show-colon>First name</ui5-label>
+					<ui5-input id="myInput" required placeholder="Enter your name"></ui5-input>
+					<p></p>
+					<ui5-label id="myLabel" for="myInput" required show-colon>First name</ui5-label>
+					<super-input v-model="a"></super-input>
+					<p></p>
+					<ui5-label id="myLabel" for="myInput" required show-colon>First name</ui5-label>
+					<super-input v-model="a"></super-input>
+					<p></p>
+					<super-button label="Test" @click="onClick"></super-button>
+					<p></p>
+				</div>
+			</ui5-tab>
+			<ui5-tab text="Details"></ui5-tab>
+			<ui5-tab text="Address" ></ui5-tab>
+			<ui5-tab text="Attachment"></ui5-tab>
+		</ui5-tabcontainer>
+
 		<div slot="footer">
 			<ui5-bar design="FloatingFooter">
 				<ui5-icon name="home" slot="startContent"></ui5-icon>
