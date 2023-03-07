ba3ebec646ead2a6cc85781e650f991f703c063b
SparkleX <xufei.sun@sap.com>
2023-03-07 15:20:29 +0800

    dev

--- ui5-vue/src/views/ORDR.vue
+++ ui5-vue/src/views/ORDR.vue
@@ -1,49 +1,71 @@
 <template>
-	<ui5-page id="page" background-design="List" style="height: 500px" floating-footer show-footer>
+	<ui5-page id="page" background-design="List" style="height: 95vh" floating-footer show-footer disable-scrolling="true">
+		<ui5-bar design="Header">
+			<ui5-label slot="startContent">Sales Order 100</ui5-label>
+			<ui5-button slot="startContent" icon="navigation-left-arrow"></ui5-button>
+			<ui5-button slot="startContent" icon="navigation-right-arrow"></ui5-button>
+
+			<ui5-button slot="endContent" design="Emphasized">Edit</ui5-button>
+			<ui5-button slot="endContent">New</ui5-button>
+			<ui5-button slot="endContent">Delete</ui5-button>
+		</ui5-bar>
+		<ui5-label>Business Partner</ui5-label>
+
 		<ui5-tabcontainer>
 			<ui5-tab text="General" selected>
-				<div class="form">
-					<div class="form-element">
-						<ui5-label id="myLabel" class="grid-item" for="myInput" required show-colon>First name</ui5-label>
+				<ui5-dynamic-side-content equal-split>
+					<div>
+						<h1>Main Content</h1>
 						<div>
-							<ui5-input id="myInput" class="grid-item" required placeholder="Enter your name"></ui5-input>
-							<ui5-input id="myInput" class="grid-item" required placeholder="Enter your name"></ui5-input>
+							<div>
+								<ui5-label id="myLabel"  for="myInput" required show-colon>First
+									name</ui5-label>
+								<div>
+									<ui5-input id="myInput"  required
+										placeholder="Enter your name"></ui5-input>
+									<ui5-input id="myInput"  required
+										placeholder="Enter your name"></ui5-input>
+								</div>
+							</div>
+							<div>
+								<ui5-label id="myLabel"  for="myInput" required show-colon>First
+									name</ui5-label>
+								<super-input v-model="a" ></super-input>
+							</div>
+							<div>
+								<ui5-label id="myLabel"  for="myInput" required show-colon>First
+									name</ui5-label>
+								<super-input v-model="a" ></super-input>
+							</div>
+							<div>
+								<super-button label="Test" @click="onClick"></super-button>
+							</div>
 						</div>
 					</div>
-					<div class="form-element">
-						<ui5-label id="myLabel" class="grid-item" for="myInput" required show-colon>First name</ui5-label>
-						<super-input v-model="a" class="grid-item"></super-input>
-					</div>
-					<div class="form-element">
-						<ui5-label id="myLabel" class="grid-item" for="myInput" required show-colon>First name</ui5-label>
-						<super-input v-model="a" class="grid-item"></super-input>
-					</div>
-					<div class="form-element">
-						<super-button label="Test" @click="onClick"></super-button>
-					</div>
-				</div>
-				<div class="form">
-					<div class="form-element">
-						<ui5-label class="grid-item" required show-colon>Right</ui5-label>
+					<div slot="sideContent">
+						<h1>Side Content</h1>
 						<div>
-							<ui5-input id="myInput" class="grid-item" required placeholder="Enter your name"></ui5-input>
-							<ui5-input id="myInput" class="grid-item" required placeholder="Enter your name"></ui5-input>
+							<ui5-label  required show-colon>Right</ui5-label>
+							<div>
+								<ui5-input id="myInput"  required
+									placeholder="Enter your name"></ui5-input>
+								<ui5-input id="myInput"  required
+									placeholder="Enter your name"></ui5-input>
+							</div>
 						</div>
 					</div>
-				</div>
+				</ui5-dynamic-side-content>
+
+
 			</ui5-tab>
 			<ui5-tab text="Details"></ui5-tab>
 			<ui5-tab text="Address"></ui5-tab>
 			<ui5-tab text="Attachment"></ui5-tab>
 		</ui5-tabcontainer>
-		<div slot="footer">
-			<ui5-bar design="FloatingFooter">
-				<ui5-icon name="home" slot="startContent"></ui5-icon>
-				<ui5-button design="Positive" slot="endContent">Agree</ui5-button>
-				<ui5-button design="Negative" slot="endContent">Decline</ui5-button>
-				<ui5-button design="Transparent" slot="endContent">Cancel</ui5-button>
-			</ui5-bar>
-		</div>
+		<ui5-bar slot="footer" design="Footer">
+			<ui5-button design="Emphasized" slot="endContent">Save</ui5-button>
+			<ui5-button slot="endContent">Cancel</ui5-button>
+		</ui5-bar>
 	</ui5-page>
 </template>
 
